3055ee9e9b2a780bcbd32a13b1cf66e2544aaf47
Divyansh Arora <141400127+Aroraboy@users.noreply.github.com>
2026-01-19 21:05:43 +0530

    feat: add Button component with styles and integrate into App

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
 import Card from "./Card.jsx";
+import Button from "./Button.jsx";  
 
 function App() {
   return(
@@ -10,7 +11,7 @@ function App() {
     <Food/>
     <Footer/>
     <Card/>
-    
+    <Button/>
     </>
   )
 }
